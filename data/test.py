f42dd8eff354707aa70a39b85fa814fcf13b0871
JeongHo Kim <63221921+altair4357@users.noreply.github.com>
2023-07-10 13:55:27 +0900

    Update test.py

--- data/test.py
+++ data/test.py
@@ -9,48 +9,37 @@ from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import mean_squared_error
 
 
-from datetime import timedelta, datetime
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import SGDRegressor
-from sklearn.metrics import mean_squared_error
-import joblib
-import numpy as np
-
-def preprocess_data(**kwargs):
+def preprocess_data():
     X, y = datasets.load_boston(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+    np.save('x_train.npy', X_train)
+    np.save('x_test.npy', X_test)
+    np.save('y_train.npy', y_train)
+    np.save('y_test.npy', y_test)
+    
 
-    kwargs['ti'].xcom_push(key='X_train', value=X_train)
-    kwargs['ti'].xcom_push(key='X_test', value=X_test)
-    kwargs['ti'].xcom_push(key='y_train', value=y_train)
-    kwargs['ti'].xcom_push(key='y_test', value=y_test)
-
-def train_model(**kwargs):
-    ti = kwargs['ti']
-
-    X_train = ti.xcom_pull(key='X_train')
-    y_train = ti.xcom_pull(key='y_train')
+def train_model():
+    x_train_data = np.load('x_train.npy')
+    y_train_data = np.load('y_train.npy')
 
     model = SGDRegressor(verbose=1)
-    model.fit(X_train, y_train)
+    model.fit(x_train_data, y_train_data)
 
-    kwargs['ti'].xcom_push(key='model', value=model)
+    joblib.dump(model, 'model.pkl')
 
-def test_model(**kwargs):
-    ti = kwargs['ti']
 
-    X_test = ti.xcom_pull(key='X_test')
-    y_test = ti.xcom_pull(key='y_test')
-    model = ti.xcom_pull(key='model')
+def test_model():
+    x_test_data = np.load('x_test.npy')
+    y_test_data = np.load('y_test.npy')
 
-    y_pred = model.predict(X_test)
-    err = mean_squared_error(y_test, y_pred)
+    model = joblib.load('model.pkl')
+    y_pred = model.predict(x_test_data)
+
+    err = mean_squared_error(y_test_data, y_pred)
 
     with open('output.txt', 'a') as f:
         f.write(str(err))
+
 
 def deploy_model():
     print('deploying model...')
@@ -76,21 +65,18 @@ dag = DAG(
 t1 = PythonOperator(
     task_id='preprocess_data',
     python_callable=preprocess_data,
-    provide_context=True,
     dag=dag,
 )
 
 t2 = PythonOperator(
     task_id='train_model',
     python_callable=train_model,
-    provide_context=True,
     dag=dag,
 )
 
 t3 = PythonOperator(
     task_id='test_model',
     python_callable=test_model,
-    provide_context=True,
     dag=dag,
 )
 
@@ -99,4 +85,5 @@ t4 = PythonOperator(
     python_callable=deploy_model,
     dag=dag,
 )
+
 t1 >> t2 >> t3 >> t4
