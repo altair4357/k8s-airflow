3e6edee13d24e14add4271d440390a7554decce0
JeongHo Kim <63221921+altair4357@users.noreply.github.com>
2023-07-12 14:45:44 +0900

    Update boston.py

--- data/boston.py
+++ data/boston.py
@@ -1,4 +1,4 @@
-# 2023.07.11 14:30
+# 2023.07.11 14:45
 
 from datetime import datetime
 from airflow import DAG
@@ -13,28 +13,28 @@ import joblib
 def preprocess_data():
     X, y = datasets.load_boston(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-    np.save('/mnt/nfs_share/default-nfs-pvc-pvc-504ae414-e42f-4ac6-b8fc-cc51cadf8ccc/x_train.npy', X_train)
-    np.save('/mnt/nfs_share/default-nfs-pvc-pvc-504ae414-e42f-4ac6-b8fc-cc51cadf8ccc/x_test.npy', X_test)
-    np.save('/mnt/nfs_share/default-nfs-pvc-pvc-504ae414-e42f-4ac6-b8fc-cc51cadf8ccc/y_train.npy', y_train)
-    np.save('/mnt/nfs_share/default-nfs-pvc-pvc-504ae414-e42f-4ac6-b8fc-cc51cadf8ccc/y_test.npy', y_test)
+    np.save('/mnt/nfs_share/x_train.npy', X_train)
+    np.save('/mnt/nfs_share/x_test.npy', X_test)
+    np.save('/mnt/nfs_share/y_train.npy', y_train)
+    np.save('/mnt/nfs_share/y_test.npy', y_test)
 
 def train_model():
-    x_train_data = np.load('/mnt/nfs_share/default-nfs-pvc-pvc-504ae414-e42f-4ac6-b8fc-cc51cadf8ccc/x_train.npy')
-    y_train_data = np.load('/mnt/nfs_share/default-nfs-pvc-pvc-504ae414-e42f-4ac6-b8fc-cc51cadf8ccc/y_train.npy')
+    x_train_data = np.load('/mnt/nfs_share/x_train.npy')
+    y_train_data = np.load('/mnt/nfs_share/y_train.npy')
     model = SGDRegressor(verbose=1)
     model.fit(x_train_data, y_train_data)
-    joblib.dump(model, '/mnt/nfs_share/default-nfs-pvc-pvc-504ae414-e42f-4ac6-b8fc-cc51cadf8ccc/model.pkl')
+    joblib.dump(model, '/mnt/nfs_share/model.pkl')
 
 def deploy_model():
     print('deploying model...')
 
 def test_model():
-    x_test_data = np.load('/mnt/nfs_share/default-nfs-pvc-pvc-504ae414-e42f-4ac6-b8fc-cc51cadf8ccc/x_test.npy')
-    y_test_data = np.load('/mnt/nfs_share/default-nfs-pvc-pvc-504ae414-e42f-4ac6-b8fc-cc51cadf8ccc/y_test.npy')
-    model = joblib.load('/mnt/nfs_share/default-nfs-pvc-pvc-504ae414-e42f-4ac6-b8fc-cc51cadf8ccc/model.pkl')
+    x_test_data = np.load('/mnt/nfs_share/x_test.npy')
+    y_test_data = np.load('/mnt/nfs_share/y_test.npy')
+    model = joblib.load('/mnt/nfs_share/model.pkl')
     y_pred = model.predict(x_test_data)
     err = mean_squared_error(y_test_data, y_pred)
-    with open('/mnt/nfs_share/default-nfs-pvc-pvc-504ae414-e42f-4ac6-b8fc-cc51cadf8ccc/output.txt', 'a') as f:
+    with open('/mnt/nfs_share/output.txt', 'a') as f:
         f.write(str(err))
 
 # Using a DAG context manager, you don't have to specify the dag property of each task
@@ -61,4 +61,5 @@ with DAG('machine_learning_workflow',
 
     # Define task dependencies
     preprocess_data_task >> train_model_task >> deploy_model_task >> test_model_task
+
 
